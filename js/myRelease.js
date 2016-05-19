86b63d33aa4dad9452194108cf72c3ebcecfa701
BG239176 <BG239176@bg239176.800best.net>
2016-05-19 19:27:51 +0800

    设置图片提交地址

--- js/myRelease.js
+++ js/myRelease.js
@@ -15,23 +15,33 @@ $(function() {
 
 $(document).ready(function(){
 	
-
+	/**
+	 *图片上传方法 
+	 */
+	function ajaxFileUpload(){
+		var formData = new FormData();
+		formData.append('file',$("#filepic")[0].files[0]);	//将文件转成二进制形式
+		$.ajax({
+			type:"post",
+			url:"http://localhost:8080/nitshare/serve/fileupload",
+			async:false,
+			contentType: false,	//这个一定要写
+			processData: false, //这个也一定要写，不然会报错
+			data:formData,
+			dataType:'text',	//返回类型，有json，text，HTML。这里并没有jsonp格式，所以别妄想能用jsonp做跨域了。
+			success:function(data){
+				alert(data);
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown, data){
+				alert(errorThrown);
+			}			
+		});
+	}
+	
     /**
      * 如果图片改变则上传图片到服务器，并返回图片地址
      */
     $("#filepic").change(function(){
-    	alert($(this).val());
-	    $("#picdiv").ajaxSubmit({
-	    	type: "POST",
-	    	url: "http://localhost:8080/nitshare/serve/uploadfile",
-	    	dataType: "jsonp",
-			jsonpCallback:'callback',
-	    	success: function(data){
-	    		alert("in");
-	    	},
-	    	error: function(XmlHttpRequest,textStatus,errorThrown){
-	    		alert("error");
-	    	}
-	    });
+    	ajaxFileUpload();
 	});
 });
---
+++ js/upload.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+	
+	function ajaxFileUpload(){
+		var formData = new FormData();
+		formData.append('file',$("#file_upload")[0].files[0]);	//将文件转成二进制形式
+		$.ajax({
+			type:"post",
+			url:"http://localhost:8080/nitshareserver/serve/fileupload",
+			async:false,
+			contentType: false,	//这个一定要写
+			processData: false, //这个也一定要写，不然会报错
+			data:formData,
+			dataType:'text',	//返回类型，有json，text，HTML。这里并没有jsonp格式，所以别妄想能用jsonp做跨域了。
+			success:function(data){
+				alert(data);
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown, data){
+				alert(errorThrown);
+			}			
+		});
+	}
+	
+	$("#upload").click(function(){
+		ajaxFileUpload();
+	});
+});
